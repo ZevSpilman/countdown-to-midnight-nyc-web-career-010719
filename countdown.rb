df00de0edeac6bd360ffae983020427189c2e5e1
Zev Spilman <zevspilman@yahoo.com>
2018-12-31 22:17:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,15 @@
 #write your code here
 
-def countdown_with_sleep(int)
+def countdown(int)
   while int > 0 
   countdown_with_sleep
-  sleep(1.second)
   puts "#{int} SECOND(S)!"
   int -= 1
 end
 puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  while int > 0
+  puts "#{int} SECOND(S)"
+  sleep()
