de30d672423b731de0f40d8d068b36386f89f850
Zev Spilman <zevspilman@yahoo.com>
2018-12-31 22:14:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 def countdown_with_sleep(amt)
-  
+  sleep(amt)
 
 
 def countdown(int)
   while int > 0 
+  countdown_with_sleep
   puts "#{int} SECOND(S)!"
   int -= 1
 end
